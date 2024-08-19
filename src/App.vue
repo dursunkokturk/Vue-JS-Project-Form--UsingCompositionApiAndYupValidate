05148d6c93e2eb6238049ca6158346a0dfaf1c6e
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-08-19 14:26:44 +0300

    composition-api-and-yup-validate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
     <div>
-
-        <!-- Header.vue Dosyasini main.js Dosyasi Uzerinden 
-              Global Olarak 
-              Component Olarak Cagiriyoruz -->
         <app-header/>
         <div class="container">
             <!-- <ContactForm /> -->
             <!-- <VeeValidation /> -->
             <YupValidation />
         </div>
-
-        <!-- Local Olarak Cagirdigimiz 
-              Footer.vue Dosyasini Component Olarak Kullaniyoruz -->
         <Footer/>
     </div>  
 </template>
 
-<!-- composition api yi Projede Kullanmak Icin 
-      script Tag lari Icinde 
-      setup Yazmak Gerekiyor -->
 <script setup>
 
-    /* Dosyanin Dizin Yolunu Belirterek Cagirma Islemi
-        Local Olarak Dosya Cagirma Islemidir */
     import Footer from '@/components/Fixed/Footer.vue'
     import ContactForm from '@/components/Contact/ContactForm.vue';
     import VeeValidation from '@/components/Contact/Vee-Validation.vue';
--- src/components/Contact/Yup-Validation.vue
+++ src/components/Contact/Yup-Validation.vue
@@ -4,13 +4,7 @@
             <label>
                 Adınız
             </label>
-
-            <!-- formSchema Degiskenine Atanan Islemler Ile Kontrol Islemini Yapiyoruz -->
             <Field name="firstName" class="form-control" placeholder="Adınızı Giriniz"/>
-            
-            <!-- as Attribute Icinde div Kullanildiginda 
-                Componentin div Elementi Olarak Islevli Halde Olmasini Saglar -->
-            <!-- v-slot Attribute Icinde Hata Mesajlarinin Bulundugu Degisken Adini Veriyoruz -->
             <ErrorMessage name="firstName" as="div" v-slot="{message}">
                 <div class="alert alert-danger">
                     {{ message }}
@@ -21,33 +15,64 @@
             <label>
                 Soyadınız
             </label>
-
-            <!-- rules Attribute Icinde Hata Mesajlarini Array Icine Aliyoruz -->
             <Field name="lastName" class="form-control" placeholder="Soyadınızı Giriniz"/>
-            
-            <!-- as Attribute Icinde div Kullanildiginda 
-                Componentin div Elementi Olarak Islevli Halde Olmasini Saglar -->
-            <!-- v-slot Attribute Icinde Hata Mesajlarinin Bulundugu Degisken Adini Veriyoruz -->
             <ErrorMessage name="lastName" as="div" v-slot="{message}">
                 <div class="alert alert-danger">
                     {{ message }}
                 </div>
             </ErrorMessage>
         </div>
+        <div class="mb-3">
+            <label>
+                Cinsiyetiniz
+            </label>
+            <div class="form-check">
+            <Field v-slot="{ field }" name="man" type="radio" :value="true">
+                <label>
+                    <input type="radio" name="terms" v-bind="field" v-model="formData.goals" :value="woman" />
+                    <p>
+                        Kadın
+                    </p>
+                </label>
+            </Field>
+            
+            <Field v-slot="{ field }" name="woman" type="radio" :value="true">
+                <label>
+                    <input type="radio" name="terms" v-bind="field" v-model="formData.goals" :value="man" />
+                    <p>
+                        Erkek
+                    </p>
+                </label>
+            </Field>
+            </div>
+        </div>
         <button class="btn btn-primary">Gönder</button>
-        
     </Form>    
 </template>
 
 <script setup>
-
-    /* Form Olusturulacak Form Olarak Kullaniliyor */
-    /* Field Form Uzerinde Bilgilerin Girildigi Input Olarak Kullaniliyor */
     import { Form,Field,ErrorMessage } from 'vee-validate';
     import * as Yup from 'yup';
+    import { reactive } from 'vue';
 
     const formSchema = Yup.object({
         firstName:Yup.string().required('Bu Alan Zorunludur.').min(6,'En Az 6 Karakter Giriniz.'),
         lastName:Yup.string().required('Bu Alan Zorunludur.').min(6,'En Az 6 Karakter Giriniz.')
     })
+
+    const formData = reactive({
+        gender:'man',
+    })
 </script>
+
+<style scoped>
+    label {
+        display: flex;
+        flex-direction: row;
+    }
+    p {
+        margin-top: 0;
+        margin-bottom: 0;
+        margin-left: 10px;
+    }
+</style>
